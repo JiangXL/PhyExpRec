390a0ed7e3f79c2b803c8a76c48136f08961a232
Chris <11510511@mail.sustc.edu.cn>
2017-06-23 22:09:49 +0800

    Add crontabs setting

--- SHT31.py
+++ SHT31.py
@@ -1,10 +1,11 @@
 #-------------------SHT31-------------------------------
 #|     Time  | Version | Contribution | Comment
 #| 2017.6.22|  0       |  fork form http://www.pibits.net/code/raspberry-pi-sht31-sensor-example.php
-#| 2017.6.16 | 0.6     | H.F.         |
+#| 2017.6.23 | 0.7     | H.F.         |
 #---------------------------------------------------------------
 import smbus
 import time
+
 
 # Get I2C bus
 bus = smbus.SMBus(1)
@@ -23,9 +24,21 @@ data = bus.read_i2c_block_data(0x44, 0x00, 6)
 temp = data[0] * 256 + data[1]
 cTemp = -45 + (175 * temp / 65535.0)
 humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+ticks = time.time()
 
 # Output data to screen
+print "Time :",ticks
 print "Temperature in Celsius is : %.2f C" %cTemp
 print "Relative Humidity is : %.2f %%RH" %humidity
 
+try:
+    record = open('/home/pi/Desktop/SHT31.txt', 'a')    # 打开文件
+    record.write(str(ticks))
+    record.write(str(cTemp))          # 添加数据到文件
+    record.write(str(humidity))
+    record.write('\n')
+finally:
+    if record:
+        record.close()
+        #GPIO.cleanup()
 # http://www.pibits.net/wp-content/uploads/2016/11/pi-and-sht31_bb.png
